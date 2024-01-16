3448a5cca1702cd7156a10d3dc989c7595a8db8a
Rosa G. Sierra Villanueva <rosagloriasv27@gmail.com>
2024-01-16 02:13:40 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const buttons = document.querySelectorAll('.learn-more-btn');
-    buttons.forEach(btn => {
-        btn.addEventListener('click', function() {
-            const flipCardInner = this.closest('.flip-card').querySelector('.flip-card-inner');
+document.addEventListener('DOMContentLoaded', function() {
+    var learnMoreButtons = document.querySelectorAll('.learn-more-btn');
+    learnMoreButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            var flipCardInner = this.closest('.flip-card').querySelector('.flip-card-inner');
             flipCardInner.classList.toggle('flipped');
+            console.log('Button clicked!');
         });
     });
 });
