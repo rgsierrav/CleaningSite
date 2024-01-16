da749409b029a19825fefe668d7bc224c6c69880
Rosa G. Sierra Villanueva <rosagloriasv27@gmail.com>
2024-01-16 13:01:53 -0500

    added flip

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var learnMoreButtons = document.querySelectorAll('.learn-more-btn');
+//     learnMoreButtons.forEach(function(button) {
+//         button.addEventListener('click', function(event) {
+//             var flipCardInner = this.closest('.flip-card').querySelector('.flip-card-inner');
+//             flipCardInner.classList.toggle('flipped');
+//             console.log('Button clicked!');
+//         });
+//     });
+// });
+
 document.addEventListener('DOMContentLoaded', function() {
-    var learnMoreButtons = document.querySelectorAll('.learn-more-btn');
-    learnMoreButtons.forEach(function(button) {
-        button.addEventListener('click', function(event) {
-            var flipCardInner = this.closest('.flip-card').querySelector('.flip-card-inner');
+    var flipButtons = document.querySelectorAll('.flip-card .learn-more-btn, .flip-card .flip-button-back');
+    var flipCardInner = document.querySelector('.flip-card-inner');
+
+    flipButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
             flipCardInner.classList.toggle('flipped');
             console.log('Button clicked!');
         });
