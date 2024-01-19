dfc4156acb3cb51f811ec7bdbb411b8158a56360
Rosa G. Sierra Villanueva <rosagloriasv27@gmail.com>
2024-01-19 11:33:42 -0500

    changed images and fixing nav bar for mobile

--- scripts.js
+++ scripts.js
@@ -47,13 +47,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const header = document.querySelector('header');
     let headerBottom = header.offsetTop + header.offsetHeight;
 
+    // Function to update headerBottom if the header changes size on resize
+    const updateHeaderBottom = () => {
+        headerBottom = header.offsetTop + header.offsetHeight;
+    };
+
+    // Add resize event listener to update headerBottom value
+    window.addEventListener('resize', updateHeaderBottom);
+
     window.addEventListener('scroll', function() {
+        const headerImage = document.getElementById('header-image'); // Assuming your header image has this ID
         if (window.scrollY > headerBottom) {
             nav.classList.add('fixed-nav');
+            headerImage.classList.add('fixed-nav-hidden');
             document.body.classList.add('fixed-nav-padding');
         } else {
             nav.classList.remove('fixed-nav');
+            headerImage.classList.remove('fixed-nav-hidden');
             document.body.classList.remove('fixed-nav-padding');
         }
-    });
+    });    
 });
+
