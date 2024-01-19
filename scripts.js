73007a48ae87621a5eef8f6f27b408120d0062dd
Rosa G. Sierra Villanueva <rosagloriasv27@gmail.com>
2024-01-19 14:32:05 -0500

    old.js

--- scripts.js
+++ scripts.js
@@ -28,34 +28,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         headerBottom = header.offsetTop + header.offsetHeight;
     };
 
-    // Debounce function to optimize performance
-    const debounce = (func, delay) => {
-        let inDebounce;
-        return function() {
-            const context = this;
-            const args = arguments;
-            clearTimeout(inDebounce);
-            inDebounce = setTimeout(() => func.apply(context, args), delay);
-        };
-    };
+    // Add resize event listener to update headerBottom value
+    window.addEventListener('resize', updateHeaderBottom);
 
-    // Add resize event listener with debounce
-    window.addEventListener('resize', debounce(updateHeaderBottom, 100));
-
-    window.addEventListener('scroll', debounce(function() {
+    window.addEventListener('scroll', function() {
         const headerImage = document.getElementById('header-image'); // Assuming your header image has this ID
         if (window.scrollY > headerBottom) {
             nav.classList.add('fixed-nav');
-            if (headerImage) {
-                headerImage.classList.add('fixed-nav-hidden');
-            }
+            headerImage.classList.add('fixed-nav-hidden');
             document.body.classList.add('fixed-nav-padding');
         } else {
             nav.classList.remove('fixed-nav');
-            if (headerImage) {
-                headerImage.classList.remove('fixed-nav-hidden');
-            }
+            headerImage.classList.remove('fixed-nav-hidden');
             document.body.classList.remove('fixed-nav-padding');
         }
-    }, 100));    
+    });    
 });
+
