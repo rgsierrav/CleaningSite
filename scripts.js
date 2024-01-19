57a7f9857a12c33e7d0598c2d34863727005de83
Rosa G. Sierra Villanueva <rosagloriasv27@gmail.com>
2024-01-19 11:43:54 -0500

    fixing nav bar

--- scripts.js
+++ scripts.js
@@ -1,27 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var flipButtons = document.querySelectorAll('.flip-card .learn-more-btn, .flip-card .flip-button-back');
-//     var flipCardInner = document.querySelector('.flip-card-inner');
-
-//     flipButtons.forEach(function(button) {
-//         button.addEventListener('click', function() {
-//             flipCardInner.classList.toggle('flipped');
-//             console.log('Button clicked!');
-//         });
-//     });
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var flipButtons = document.querySelectorAll('.learn-more-btn');
-
-//     flipButtons.forEach(function(button) {
-//         button.addEventListener('click', function() {
-//             // Find the closest ancestor which is a flip-card
-//             var flipCardInner = this.closest('.flip-card').querySelector('.flip-card-inner');
-//             flipCardInner.classList.toggle('flipped');
-//         });
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     var flipCards = document.querySelectorAll('.flip-card');
 
